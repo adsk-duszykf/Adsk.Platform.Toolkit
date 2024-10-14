92c86b1694b21bc847086284931e1929ea35a902
adsk-duszykf <frederic.duszyk@autodesk.com>
2024-10-14 17:06:43 +0200

    chore(auth): update Kiota 19.1

--- Autodesk.Authentication/GeneratedCode/Models/Granttype.cs
+++ Autodesk.Authentication/GeneratedCode/Models/Granttype.cs
@@ -3,8 +3,8 @@ using System.Runtime.Serialization;
 using System;
 namespace Autodesk.Authentication.Models
 {
-    [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.16.0")]
     /// <summary>Enum for grant types</summary>
+    [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     public enum Granttype
     {
         [EnumMember(Value = "client_credentials")]
